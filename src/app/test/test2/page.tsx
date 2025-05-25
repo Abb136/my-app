cbc13b37e368129e1982daea6a3d8b6cb731bb08
Traner110 <117926723+Traner110@users.noreply.github.com>
2025-05-25 18:32:36 +0300

    acb

--- src/app/test/test2/page.tsx
+++ src/app/test/test2/page.tsx
@@ -12,6 +12,7 @@ return <>hello world,
 
 <h1>Hadoken</h1>
 <Image alt="tttt" src={'https://raw.githubusercontent.com/pmndrs/zustand/HEAD/docs/bear.jpg'} width={250} height={250}/>
+<Image alt="ttt" src={'https://pohcdn.com/sites/default/files/styles/paragraph__text_gallery__tg_image_2/public/text_gallery/Kuwait-city-3.jpg'} width={350} height={350}/>
  {x.movie}
  </>
 }   
