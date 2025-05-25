261bfa5467d0d3613e2f1494e1322de337ef2926
Traner110 <117926723+Traner110@users.noreply.github.com>
2025-05-25 18:45:23 +0300

    ctbat

--- src/app/test/test2/page.tsx
+++ src/app/test/test2/page.tsx
@@ -12,7 +12,12 @@ return <>hello world,
 
 <h1>Hadoken</h1>
 <Image alt="tttt" src={'https://raw.githubusercontent.com/pmndrs/zustand/HEAD/docs/bear.jpg'} width={250} height={250}/>
-<Image alt="ttt" src={'https://pohcdn.com/sites/default/files/styles/paragraph__text_gallery__tg_image_2/public/text_gallery/Kuwait-city-3.jpg'} width={350} height={350}/>
+<Image
+      src="https://pohcdn.com/sites/default/files/styles/para…/public/text_gallery/Kuwait-city-3.jpg"
+      width={500}
+      height={500}
+      alt="tbat"
+    />
  {x.movie}
  </>
 }   
